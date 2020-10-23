5fa8f1ae153cec2b37ea22cf531fae279073c034
Anthony <anthony.porter27@gmail.com>
2020-10-23 09:27:32 +0100

    video embedded

--- clock.js
+++ clock.js
@@ -9,7 +9,16 @@ function GetClock() {
         if(nMin<=9) nMin="0"+nMin;
         if(nSec<=9) nSec="0"+nSec;
 
-    let clocktext=""+tday[nDay]+", "+tmonth[nMonth]+" "+nDate+" "+nHour+":"+nMin+":"+nSec+"";
+        let now = new Date();
+        let hrs = now.getHours();
+        let msg = "";
+
+        if (hrs >  0) msg = "Morning Riser! "; // REALLY early
+        if (hrs >  6) msg = "Good morning ";      // After 6am
+        if (hrs > 12) msg = "Good afternoon ";    // After 12pm
+        if (hrs > 17) msg = "Good evening ";      // After 5pm
+        if (hrs > 22) msg = "Go to bed! ";
+    let clocktext=msg+"it's "+nHour+":"+nMin+":"+nSec+" "+tday[nDay]+", "+tmonth[nMonth]+" "+nDate;
     document.getElementById('clockbox').innerHTML=clocktext;
     }
 
